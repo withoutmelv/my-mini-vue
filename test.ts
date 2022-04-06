096254070ae1dd505350d3239c83bc68add34393
Withoutmelv <781033189@qq.com>
2022-04-06 16:50:06 +0800

    feat: 复习ref和computed的实现

--- test.ts
+++ test.ts
@@ -1,12 +1,12 @@
-import { hasOwn, isArray, isChanged, isIntegerKey, isObject } from "@vue/shared";
+import { hasOwn, isArray, isChanged, isFunction, isIntegerKey, isObject } from "@vue/shared";
 import { trigger } from "packages/reactivity/src/effect";
 // import { track } from "packages/reactivity/src/effect";
 import { TrackOpTypes, TriggerOpTypes } from "packages/reactivity/src/operators";
 
 let uid = 0;
-const effectStack = [];
-let activeEffect;
-const createReactiveEffect = (fn, options) => {
+const effectStack: any[] = [];
+let activeEffect: { deps: any[]; };
+const createReactiveEffect = (fn: () => any, options: any) => {
     const effect = () => {
         try {
             effectStack.push(effect);
@@ -21,11 +21,14 @@ const createReactiveEffect = (fn, options) => {
     effect._isEffect = true;
     effect.options = options;
     effect.raw = fn;
+    /**
+     * @type {never[]}
+     */
     effect.deps = [];
     return effect;
 }
 
-const effect = (fn, options) => {
+const effect = (fn: any, options: { lazy: any; }) => {
     const effect = createReactiveEffect(fn, options);
     if (!options.lazy) {
         effect();
@@ -34,7 +37,7 @@ const effect = (fn, options) => {
 }
 
 const targetMap = new WeakMap;
-const track = (target, type, key) => {
+const track = (target: object, type: TrackOpTypes, key: string) => {
     if (activeEffect) {
         let depsMap = targetMap.get(target);
         if (!depsMap) {
@@ -51,7 +54,7 @@ const track = (target, type, key) => {
 
 
 const createGetter = (isReadonly = false, isShallow = false) => {
-    return function get(target, key, receiver) {
+    return function get(target: object, key: PropertyKey, receiver: any) {
         let res = Reflect.get(target, key, receiver);
         if (!isReadonly) {
             track(target, TrackOpTypes.GET, key);
@@ -102,7 +105,7 @@ const createGetter = (isReadonly = false, isShallow = false) => {
 //         })
 
 
-const trigger = (target, type, key, value, oldval) => {
+const trigger = (target: object, type: TriggerOpTypes, key: string | undefined, value: undefined, oldval: undefined) => {
     const depsMap = targetMap.get(target);
     if (depsMap) {
         const effects = new Set();
@@ -145,7 +148,7 @@ const trigger = (target, type, key, value, oldval) => {
 }
 
 const  createSetter = (isReadonly = false, isShallow = false) => {
-    return function set (target, key, value, receiver) {
+    return function set (target: string | object, key: PropertyKey, value: any, receiver: any) {
         // const oldval = target[key];
         // const hasKey = isArray(target) && isIntegerKey(key) ? Number(key) < target.length : hasOwn(target, key);
         // if (!hasKey) {
@@ -214,7 +217,7 @@ const shallowReadonlyHandler = {
 
 const reactiveMap = new WeakMap;
 const readonlyMap = new WeakMap;
-const createReactiveObject = (target, baseHandler, isReadonly = false, isShallow = false) => {
+const createReactiveObject = (target: object, baseHandler: ProxyHandler<any>, isReadonly = false, isShallow = false) => {
     if (!isObject(target)) {
         return target;
     }
@@ -229,20 +232,125 @@ const createReactiveObject = (target, baseHandler, isReadonly = false, isShallow
 }
 
 
-const reactive = (obj) => {
+const reactive = (obj: any) => {
     return createReactiveObject(obj, mutableHandler);
 }
 
-const shallowReactive = (obj) => {
+const shallowReactive = (obj: any) => {
     return createReactiveObject(obj, shallowReactiveHandler, false, true);
 }
 
-const readonly = (obj) => {
+const readonly = (obj: any) => {
     return createReactiveObject(obj, readonlyHandler, true, false);
 }
 
-const shallowReadonly = (obj) => {
+const shallowReadonly = (obj: any) => {
     return createReactiveObject(obj, shallowReadonlyHandler, true, true);
 }
 
 
+
+
+export function ref(value: any) {
+    return createRef(value);
+}
+
+export function shallowRef(value: any) {
+    return createRef(value, true);
+}
+
+const createRef = (value: any, shallow = false) => {
+    return new refImpl(value, shallow);
+}
+
+const convert = (value: any) => isObject(value) ? reactive(value) : value;
+
+class refImpl {
+    private _value: any;
+    public readonly __v_isRef = true;
+    constructor(private _rawValue: any, private readonly _shallow: boolean) {
+        this._value = _shallow ? _rawValue : convert(_rawValue);
+    }
+
+    get value() {
+        track(this, TrackOpTypes.GET, 'value');
+        return this._value;
+    }
+
+    set value(newValue) {
+        if (isChanged(this._rawValue, newValue)) {
+            this._rawValue = newValue;
+            this._value = this._shallow ? this._rawValue : convert(this._rawValue);
+            trigger(this, TriggerOpTypes.SET, 'value', newValue, undefined);
+        }
+    }
+}
+
+
+
+export function toRef(object: any, key: any) {
+    return new ObjectRefImpl(object, key);
+}
+
+class ObjectRefImpl {
+    public readonly __v_isRef = true;
+    constructor(private readonly _object: any, private readonly _key: any) {
+
+    }
+
+    get value() {
+        return this._object[this._key];
+    }
+
+    set value(newValue) {
+        this._object[this._key] = newValue;
+    }
+}
+
+
+
+export function computed(getterOrOptions: { get: any; set: any; }) {
+    let getter;
+    let setter;
+    if (isFunction(getterOrOptions)) {
+        getter = getterOrOptions;
+        setter = () => {
+            console.log("computed is readonly!");
+        }
+    } else {
+        getter = getterOrOptions.get;
+        setter = getterOrOptions.set;
+    }
+    return new ComputedRefImpl(getter, setter);
+}
+
+class ComputedRefImpl {
+    private _value: any;
+    private _dirty = true;
+    public readonly effect;
+    public readonly __v_isRef = true;
+    constructor(getter: any, _setter: any) {
+        this.effect = effect(getter, {
+            lazy: true,
+            scheduler: (effect: any) => {
+                if (!this._dirty) {
+                    this._dirty = true;
+                    trigger(this, TriggerOpTypes.SET, 'value')
+                }
+            }
+        });
+    }
+
+    get value() {
+        if (this._dirty) {
+            this._value = this.effect();
+            this._dirty = false;
+        }
+        track(this, TrackOpTypes.GET, 'value');
+        return this._value;
+    }
+
+    set value(newval) {
+        this._setter(newval);
+    }
+}
